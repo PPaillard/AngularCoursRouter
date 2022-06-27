986b6ef997f3c5049bab0b1e3073e71346e02fd4
Pierre <53476143+RoYaLSleT@users.noreply.github.com>
2022-06-27 15:52:08 +0200

    creation des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    redirectTo : "/home",
+    pathMatch : "full"
+  },
+  {
+    path : "home",
+    component : HomeComponent
+  },
+  {
+    path: "student/:studentName",
+    component : StudentComponent
+  },
+  {
+    path: "**",
+    component : NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './student/student.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudentComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
